c7fd0de92ea69cdbca17435c523ddd878b8ebd3d
Chris Wilding <hello@chriswilding.co.uk>
2022-01-05 17:24:35 +0000

    Types of Values
    Tagged Unions
    Lox Values and C Values
    Dynamically Typed Numbers
    Two New Types

--- src/value.h
+++ src/value.h
@@ -1,17 +1,43 @@
 #ifndef CLOX_VALUE_H
 #define CLOX_VALUE_H
 
-typedef double Value;
+#include <stdbool.h>
+
+typedef enum {
+  VAL_BOOL,
+  VAL_NIL,
+  VAL_NUMBER,
+} ValueType;
 
 typedef struct {
-    int capacity;
-    int count;
-    Value* values;
+  ValueType type;
+  union {
+    bool boolean;
+    double number;
+  } as;
+} Value;
+
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+
+typedef struct {
+  int capacity;
+  int count;
+  Value *values;
 } ValueArray;
 
-void freeValueArray(ValueArray* array);
-void initValueArray(ValueArray* array);
+void freeValueArray(ValueArray *array);
+void initValueArray(ValueArray *array);
 void printValue(Value value);
-void writeValueArray(ValueArray* array, Value value);
+bool valuesEqual(Value a, Value b);
+void writeValueArray(ValueArray *array, Value value);
 
 #endif
